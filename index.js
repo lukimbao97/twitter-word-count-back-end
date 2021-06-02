303b6c621903152240a6de49e3b4862b64f00f75
Lu Kim bao <baolk@Lus-MacBook-Air.local>
2021-06-02 19:32:21 +0700

    Add cors middleware

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const tweets = require("./routes/tweets");
 const home = require("./routes/home");
+const cors = require("cors");
+const app = express();
 
 app.use(express.json());
 app.use("/api/tweets", tweets);
 app.use("/", home);
+app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(
   "mongodb+srv://dbUser:lkb123456789@cluster0.0lvny.mongodb.net/TwitterWordCountDatabase?retryWrites=true&w=majority",
